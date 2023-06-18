3e657f0f19774cfdfa2bf58ce26b2688cadd0084
HoangLeCoder <hle070302@gmail.com>
2023-06-18 16:04:23 +0700

    Add CarCard component

--- utils/index.ts
+++ utils/index.ts
@@ -1,14 +1,15 @@
 export async function fetchCars() {
     const headers = {
-            "X-RapidAPI-Key":
-                "2bc7c49e5dmshc3563c618593a8bp188811jsn87aa546c59eb",
-            "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+        'X-RapidAPI-Key': '2bc7c49e5dmshc3563c618593a8bp188811jsn87aa546c59eb',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     };
 
     const response = await fetch(
-        "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+        'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
         {headers: headers, }
     );
 
     const result = await response.json(); 
+
+    return result;
 }
