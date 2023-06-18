629f59be99bac608a89bd742295bb8b62f63fda5
HoangLeCoder <hle070302@gmail.com>
2023-06-18 15:35:27 +0700

    Set up API from RapidAPI

---
+++ utils/index.ts
@@ -0,0 +1,14 @@
+export async function fetchCars() {
+    const headers = {
+            "X-RapidAPI-Key":
+                "2bc7c49e5dmshc3563c618593a8bp188811jsn87aa546c59eb",
+            "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    };
+
+    const response = await fetch(
+        "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+        {headers: headers, }
+    );
+
+    const result = await response.json(); 
+}
