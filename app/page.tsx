01a9890b3fe76da62378f15c8aa8fe5a347bd59e
HoangLeCoder <hle070302@gmail.com>
2023-06-19 15:51:04 +0700

    Complete ShowMore function

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fetchCars } from "@/utils";
 import { fuels, yearsOfProduction } from "@/constants";
 
-export default async function Home( {searchParams}) {
+export default async function Home({ searchParams }) {
     const allCars = await fetchCars({
-        manufacturer: searchParams.manufacturer || '',
+        manufacturer: searchParams.manufacturer || "",
         year: searchParams.year || 2022,
-        fuel: searchParams.fuel || '',
+        fuel: searchParams.fuel || "",
         limit: searchParams.limit || 10,
-        model: searchParams.model || '',
+        model: searchParams.model || "",
     });
     const isDataEmpty =
         !Array.isArray(allCars) || allCars.length < 1 || !allCars;
@@ -27,8 +27,11 @@ export default async function Home( {searchParams}) {
                 <div className="home__filters">
                     <SearchBar />
                     <div className="home__filter-container">
-                        <CustomFilter title="fuel" options={fuels}/>
-                        <CustomFilter title="year" options={yearsOfProduction}/>
+                        <CustomFilter title="fuel" options={fuels} />
+                        <CustomFilter
+                            title="year"
+                            options={yearsOfProduction}
+                        />
                     </div>
                 </div>
 
@@ -39,6 +42,11 @@ export default async function Home( {searchParams}) {
                                 <CarCard car={car} />
                             ))}
                         </div>
+
+                        <ShowMore
+                            pageNumber={(searchParams.limit || 10) / 10}
+                            isNext={(searchParams.limit || 10) > allCars.length}
+                        />
                     </section>
                 ) : (
                     <div className="home__error-container">
